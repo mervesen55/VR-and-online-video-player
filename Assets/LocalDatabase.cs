b83f80616b14bd94ca76eb5d6e2688bb5f9895df
Merve Şensoy <merveesensoy@hotmail.com>
2021-10-07 16:14:47 +0300

    Add

---
+++ Assets/LocalDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Local Database", menuName = "LocalDatabase")]
+public class LocalDatabase : ScriptableObject
+{
+    public string Email;
+    public string Password;
+    public string VideoKind;
+
+   
+
+
+}
---
+++ Assets/OnlineVideoLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class OnlineVideoLoader : MonoBehaviour
+{
+
+    public VideoPlayer videoPlayer;
+    public string videoUrl;
+    public LocalDatabase localDatabase;
+    public string videoUrl1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.url = videoUrl1+videoUrl;
+        Debug.Log(videoPlayer.url);
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.Prepare();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
